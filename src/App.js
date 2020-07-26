78bb59c3fc1245df870c9694a0fbd6f7fd5560db
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-26 01:10:40 -0400

    ToTop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom'
 import PrivateRoute from './components/PrivateRoute.js';
+
+//Components
 import Header from './components/Header.js';
+import ToTop from './components/ToTop.js';
 import Footer from './components/Footer.js';
+
+//Styles
+import './App.css';
 
 function App() {
   return (
     <Router>
       <Header />
+
       <Switch>
-         <PrivateRoute path='/user' /> 
+        <PrivateRoute path='/user' /> 
         {/* <Route path='/login' component={Login} /> */}
         {/* <Route component={Login}/> */}
-        <Route exact path='/'/>
+        <Route exact path='/' />  
       </Switch>
+        
+      <ToTop />
       <Footer />
     </Router>
   );
---
+++ src/components/ToTop.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function ToTop() {
+  return (
+    <Link to='#' data-uk-totop data-uk-scroll>
+    </Link>
+  )
+}
