4e1e94f620126006c3cdb7e88d6f7c206b1f01e6
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-25 21:41:57 -0400

    Structure & Dependency Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom'
+import PrivateRoute from './components/PrivateRoute.js';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
-    <div>
-      app
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+         <PrivateRoute path='/user' /> 
+        {/* <Route path='/login' component={Login} /> */}
+        {/* <Route component={Login}/> */}
+        <Route exact path='/'/>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
