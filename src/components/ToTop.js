291820866fbaaf648b3c82407c2ff7b0cb594212
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-26 01:45:12 -0400

    Header

--- src/components/ToTop.js
+++ src/components/ToTop.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 export default function ToTop() {
   return (
-    <Link to='#' data-uk-totop data-uk-scroll>
+    <Link to='#' data-uk-totop data-uk-scroll className='to-top'>
+      <i class="fa fa-chevron-square-up fa-2x uk-text-primary"></i>
     </Link>
   )
 }
