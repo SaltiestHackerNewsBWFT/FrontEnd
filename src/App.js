8e6fcfcfd37a11b04e6066ffa37f678cefc4e9d9
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-27 10:42:34 -0400

    Initial Setup of Home Page

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
         <PrivateRoute path='/user' /> 
         {/* <Route path='/login' component={Login} /> */}
         {/* <Route component={Login}/> */}
+        
         <Route exact path='/' component={PageHome} /> 
         <Route exact path='/trending' component={PageTrending} /> 
         <Route exact path='/show' component={PageShow} /> 
