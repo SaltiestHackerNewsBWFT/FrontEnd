bf8024558030b19c58ac83b71805b2e79f8f2dca
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-27 13:14:27 -0400

    Format Date

--- src/components/PageHome.js
+++ src/components/PageHome.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import ReactHtmlParser from 'react-html-parser';
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
 
 export default function PageHome() {
   const [hnAll, setHnAll] = useState([]);
+  let timeAgo;
 
   useEffect(() => {
     axios.get('https://hackernewsbw31.herokuapp.com/api/comments')
@@ -28,14 +31,15 @@ export default function PageHome() {
                   <article className='uk-comment'>
                     <header className='uk-comment-header'>
                       <div className='uk-grid-medium uk-flex-middle' data-uk-grid>
-                        <div className='uk-width-auto'>
+                        {/* <div className='uk-width-auto'>
                           <img className='uk-comment-avatar' src='images/avatar.jpg' width='80' height='80' alt=''/>
-                        </div>
+                        </div> */}
                         <div className='uk-width-expand'>
                           <h4 className='uk-comment-title uk-margin-remove'><Link className='uk-link-reset' to='#'>{item.author}</Link></h4>
                           <ul className='uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top'>
-                            <li><Link to='#'>{item.created_at}</Link></li>
+                            <li><Link className='uk-text-lowercase' ><i className="fad fa-clock"></i> {timeAgo = formatDistanceToNow(item.created_at_i * 1000)} ago</Link></li>
                             <li><Link to='#'>Reply</Link></li>
+                            <li><a href={`https://news.ycombinator.com/item?id=${item.story_id}`} target='_blank'> go to</a></li>
                           </ul>
                         </div>
                       </div>
