3aaafddf16370b22a60b2e69fb2d3118ef01516a
Nathan Galt <adiabatic@users.noreply.github.com>
2020-07-27 10:27:45 -0700

    Get a reasonable-looking container

--- src/components/PageLogin.js
+++ src/components/PageLogin.js
@@ -7,21 +7,24 @@ export default function PageLogin() {
   }
   
   return (
-    <>
+    <div className='uk-container uk-container-center'>
       <form className='uk-form-stacked' onSubmit={onSubmit}>
-      <div class='form-group'>
+      <div className='form-group'>
           <label className='uk-form-label' for='inputEmail'>E-mail address</label>
           <div className='uk-form-controls'>
             <input type='email' name='email' id='inputEmail' className='uk-input'/>
           </div>
         </div>
-        <div class='form-group'>
+        <div className='form-group'>
           <label className='uk-form-label' for='inputPassword'>Password</label>
           <div className='uk-form-controls'>
             <input type='password' name='password' id='inputPassword' className='uk-input'/>
           </div>
         </div>
+        <div className='form-group'>
+          <input type='submit' value='Log in' className='uk-button uk-button-primary uk-width-1-1'/>
+        </div>
       </form>
-    </>
+    </div>
   )
 }
