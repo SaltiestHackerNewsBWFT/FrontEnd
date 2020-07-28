4da80a3a66a41511bb70eaf094d1cdca1cc5b0e1
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-28 12:45:47 -0400

    Page Trending

--- src/components/PageTrending.js
+++ src/components/PageTrending.js
@@ -1,10 +1,29 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { axiosHackerNews } from '../utils/axiosHackerNews';
+import ItemCard from './ItemCard';
 
 export default function PageTrending() {
+  const [trendingAll, setTrendingAll] = useState([]);
+  
+  useEffect(() => {
+    axiosHackerNews('topstories.json')
+      .then(response => {
+        setTrendingAll(response.data);
+        console.log(response);
+      });
+  }, [])
+
   return (
-    <div>
-      Trending
+    <div className='uk-section uk-section-small'> 
+      <div className='uk-container'>
+        {
+          trendingAll.map(item => {
+            return (
+              <ItemCard id={item} key={item}/>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
