b6a279fbecf0fd89b20504b8a9bef4bbf723523b
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-28 15:16:37 -0400

    Page Poll with Access Denied

--- src/components/PagePolls.js
+++ src/components/PagePolls.js
@@ -1,10 +1,32 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { axiosHackerNews } from '../utils/axiosHackerNews';
+import ItemCard from './ItemCard';
 
 export default function PagePolls() {
+  const [pollAll, setPollAll] = useState([]);
+  
+  useEffect(() => {
+    axiosHackerNews('pollstories.json')
+      .then(response => {
+        setPollAll(response.data);
+        console.log(response);
+      })
+      .catch(error => {
+        console.log('axios error: ', error);
+      })
+  }, [])
+
   return (
-    <div>
-      Polls
+    <div className='uk-section uk-section-small'> 
+      <div className='uk-container'>
+        {
+          pollAll.map(item => {
+            return (
+              <ItemCard id={item} key={item}/>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
