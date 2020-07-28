789329712a7ba56d9c4d4d16c58f58aef55aea99
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-28 12:44:39 -0400

    Page Show

--- src/components/PageShow.js
+++ src/components/PageShow.js
@@ -1,10 +1,29 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { axiosHackerNews } from '../utils/axiosHackerNews';
+import ItemCard from './ItemCard';
 
 export default function PageShow() {
+  const [showAll, setShowAll] = useState([]);
+  
+  useEffect(() => {
+    axiosHackerNews('showstories.json')
+      .then(response => {
+        setShowAll(response.data);
+        console.log(response);
+      });
+  }, [])
+
   return (
-    <div>
-      Show
+    <div className='uk-section uk-section-small'> 
+      <div className='uk-container'>
+        {
+          showAll.map(item => {
+            return (
+              <ItemCard id={item} key={item}/>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
