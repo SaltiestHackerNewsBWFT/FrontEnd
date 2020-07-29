cde4832cf84a99028067e41cd4029658ebbd5323
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-29 16:51:39 -0400

    Favorites Tweaks

--- src/components/user/Favorites.js
+++ src/components/user/Favorites.js
@@ -6,20 +6,22 @@ export default function Favorites(){
   
   const [favorites, setFavorites] = useState([]);
   
-  // useEffect(() => {
-  //   axiosWithAuth()
-  //     .get()
-  //     .then()
-  //     .catch()
-    
-  // })
-  console.log(favorites);
+  useEffect(() => {
+    axiosWithAuth()
+      .get('https://hackernewsbw31.herokuapp.com/api/favorites')
+      .then(response => {
+        // setFavorites({ ...response.data });
+        console.log('response for favorites: ', response)
+      })
+      .catch(error => console.log(error))
+  },[])
+  
   return (
     <div>
       Favorites
-      {favorites.map(item =>
+      {/* {favorites.map(item =>
         <ItemCard key={item.id} item={item} />
-      )} 
+      )}  */}
     </div>
   )
 }
