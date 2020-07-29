b676e05cdce1d3192e362173217c53e200701857
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-28 20:09:06 -0400

    Commenting Out Poll (Errors)

--- src/components/PagePolls.js
+++ src/components/PagePolls.js
@@ -5,27 +5,27 @@ import ItemCard from './ItemCard';
 export default function PagePolls() {
   const [pollAll, setPollAll] = useState([]);
   
-  useEffect(() => {
-    axiosHackerNews('pollstories.json')
-      .then(response => {
-        setPollAll(response.data);
-        console.log(response);
-      })
-      .catch(error => {
-        console.log('axios error: ', error);
-      })
-  }, [])
+  // useEffect(() => {
+  //   axiosHackerNews('pollstories.json')
+  //     .then(response => {
+  //       setPollAll(response.data);
+  //       console.log(response);
+  //     })
+  //     .catch(error => {
+  //       console.log('axios error: ', error);
+  //     })
+  // }, [])
 
   return (
     <div className='uk-section uk-section-small'> 
       <div className='uk-container'>
-        {
+        {/* {
           pollAll.map(item => {
             return (
               <ItemCard id={item} key={item}/>
             )
           })
-        }
+        } */}
       </div>
     </div>
   )
