921cb8b21f2d0f73a894ed8da14e1fa0ec97b245
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-29 16:35:46 -0400

    Profile Page Form

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -2,8 +2,49 @@ import React from 'react';
 
 export default function UserProfile() {
   return (
-    <div>
-      my profile
+    <div className='uk-section uk-section-small'>
+      <div className='uk-container uk-flex uk-flex-center'>
+        <form className='uk-card uk-card-default uk-card-body uk-width-large'>
+          <legend className='uk-text-center uk-text-primary uk-h2'>Profile</legend>
+          <div className='uk-margin uk-flex uk-flex-center'>
+            <img src='' width='132px' height='132px' alt='profile' style={{border: '2px solid #ff6600', minWidth: '132px', minHeight: '132px'}}/>
+          </div>
+          <div class='uk-margin'>
+            <div className='uk-flex uk-flex-center' data-uk-form-custom>
+              <input type='file'/>
+              <button className='uk-button uk-button-default uk-button-small' type='button' tabindex='-1'>Upload Image</button>
+            </div>
+          </div>
+          <div className='uk-margin'>
+            <label forHtml='email' className='uk-text-primary'>User Name</label>
+            <input 
+              className='uk-input'
+              name='username'
+              placeholder='User Name'
+            />
+          </div>
+          <div className='uk-margin'>
+            <label forHtml='email' className='uk-text-primary'>Email</label>
+            <input 
+              className='uk-input'
+              name='email'
+              placeholder='Email'
+            />
+          </div>
+          <div className='uk-margin'>
+            <label forHtml='email' className='uk-text-primary'>Password</label>
+            <input 
+              className='uk-input'
+              name='password'
+              placeholder='Password'
+            />
+          </div>
+          <div className='uk-margin-medium'>
+            <button className='uk-button uk-button-secondary'>Cancel</button>
+            <button className='uk-button uk-button-primary uk-float-right'>Update</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
