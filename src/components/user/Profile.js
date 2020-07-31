ae2cc08204c6f7a98a146e882492e388aad81899
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-31 00:43:58 -0400

    Hide Profile Image

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -6,7 +6,7 @@ export default function UserProfile() {
       <div className='uk-container uk-flex uk-flex-center'>
         <form className='uk-card uk-card-default uk-card-body uk-width-large'>
           <legend className='uk-text-center uk-text-primary uk-h2'>Profile</legend>
-          <div className='uk-margin uk-flex uk-flex-center'>
+          {/* <div className='uk-margin uk-flex uk-flex-center'>
             <img src='' width='132px' height='132px' alt='profile' style={{border: '2px solid #ff6600', minWidth: '132px', minHeight: '132px'}}/>
           </div>
           <div className='uk-margin'>
@@ -14,7 +14,7 @@ export default function UserProfile() {
               <input type='file'/>
               <button className='uk-button uk-button-default uk-button-small' type='button' tabIndex='-1'>Upload Image</button>
             </div>
-          </div>
+          </div> */}
           <div className='uk-margin'>
             <label htmlFor='username' className='uk-text-primary'>Username (optional)</label>
             <input 
