98d2cb29670a3bd713f600bfbaf6174c1ae23a90
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-30 23:55:56 -0400

    Algolia http vs https

--- src/utils/axiosAlgolia.js
+++ src/utils/axiosAlgolia.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const axiosAlgolia = (props) => {
   return (
-    axios.get(`http://hn.algolia.com/api/v1/${props}`)
+    axios.get(`https://hn.algolia.com/api/v1/${props}`)
   )
 }
